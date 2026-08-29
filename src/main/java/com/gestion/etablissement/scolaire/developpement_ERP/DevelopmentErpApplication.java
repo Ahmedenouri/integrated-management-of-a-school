def49e49c9c582c0da7ec90aa4bff75621eff524
ahmed ennouri <ahmedenouri7@gmail.com>
2026-08-29 16:24:50 +0100

    configuration MySql

---
+++ src/main/java/com/gestion/etablissement/scolaire/developpement_ERP/DevelopmentErpApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.etablissement.scolaire.developpement_ERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gestion/etablissement/scolaire/developpement_ERP/DevelopmentErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.etablissement.scolaire.developpement_ERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
