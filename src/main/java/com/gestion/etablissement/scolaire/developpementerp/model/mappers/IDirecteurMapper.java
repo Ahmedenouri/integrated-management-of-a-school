5b268bc754dc7d8914054ff9855f99f18c650883
ahmed ennouri <ahmedenouri7@gmail.com>
2026-08-31 16:35:30 +0100

    Add mapping methods for Directeur in IDirecteurMapper
    - Introduce mapping methods to convert between DirecteurRequest and Directeur entities.
    - Add methods for mapping Directeur to DirecteurResponce and for mapping lists of Directeurs.
    - Implement updateFromRequest method to update Directeur entities while ignoring null properties.

--- src/main/java/com/gestion/etablissement/scolaire/developpementerp/model/mappers/IDirecteurMapper.java
+++ src/main/java/com/gestion/etablissement/scolaire/developpementerp/model/mappers/IDirecteurMapper.java
@@ -1,4 +1,23 @@
 package com.gestion.etablissement.scolaire.developpementerp.model.mappers;
 
+import com.gestion.etablissement.scolaire.developpementerp.model.dtos.dtoRequests.DirecteurRequest;
+import com.gestion.etablissement.scolaire.developpementerp.model.dtos.dtoResponce.DirecteurResponce;
+import com.gestion.etablissement.scolaire.developpementerp.model.entities.Directeur;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface IDirecteurMapper {
+    Directeur map(DirecteurRequest directeurRequest);
+
+    DirecteurResponce map(Directeur directeur);
+
+    List<DirecteurResponce> mapList(List<Directeur> directeurs);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateFromRequest(DirecteurRequest directeurRequest, @MappingTarget Directeur directeur);
 }
